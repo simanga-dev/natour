5134d25876209b87ab353b2b5c894db3fbbeec31
Hendry <h3khoza@gmail.com>
2021-02-21 14:10:33 +0000

    manage to connect to a database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app')
 
-const port = 3000;
+dotenv.config({ path: './config.env' })
+
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE_LOCAL
+
+mongoose.connect(DB, {
+  userNewUrParser: true,
+  useCreteIndex: true,
+  useFindAndModify: false
+}).then(con => {
+    console.log(!con.connection);
+    console.log('DB connection successful!');
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
 })
